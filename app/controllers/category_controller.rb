88063aef00e7fcf3b1ae8f2d34053045d5112258
haruki1125 <haruki1125@users.noreply.github.com>
2021-01-20 11:16:27 +0900

    commit

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -2,4 +2,9 @@ class CategoryController < ApplicationController
   def index
     @shops = Shop.where(category_id: params[:category_id])
   end
+  def destroy
+    @note = Shop.find(params[:id])
+    @note.destroy
+    redirect_to root_path
+  end
 end
--- app/controllers/price_controller.rb
+++ app/controllers/price_controller.rb
@@ -2,4 +2,10 @@ class PriceController < ApplicationController
   def index
     @shops = Shop.where(price_id: params[:price_id])
   end
+
+  def destroy
+    @note = Shop.find(params[:id])
+    @note.destroy
+    redirect_to root_path
+  end
 end
